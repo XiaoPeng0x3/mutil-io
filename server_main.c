b15cd723f49ffdddbee0e634744558b97ac83e97
XiaoPeng <zxpcs0x3@gmail.com>
2025-07-20 13:46:19 +0800

    feat:使用frok完成多进程服务器

--- server_main.c
+++ server_main.c
@@ -3,6 +3,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <arpa/inet.h>
+#include <unistd.h>
+#include <wait.h>
 
 int main(int argc, char *argv[]) {
     if (argc != 3) {
@@ -26,14 +28,24 @@ int main(int argc, char *argv[]) {
         int client_fd = accept_client(server_fd, &client_addr);
         if (client_fd < 0) continue;
 
-        char buffer[1024];
-        read_data(client_fd, buffer, sizeof(buffer));
-        printf("收到: %s\n", buffer);
-
-        send_data(client_fd, "你好，客户端！\n", 18);
-        close(client_fd);
+        pid_t pid = fork();
+        if (pid < 0) {
+            perror("创建进程失败！");
+            exit(1);
+        } else if (pid == 0) {
+            // 子进程处理请求
+            char buffer[1024];
+            read_data(client_fd, buffer, sizeof(buffer));
+            printf("收到: %s\n", buffer);
+            send_data(client_fd, "你好，客户端！\n", 18);
+            close(client_fd);
+            exit(0);  // 子进程必须退出
+        } else {
+            // 父进程继续监听下一个连接，并清理子进程
+            close(client_fd); // 父进程不处理这个 client_fd
+            while (waitpid(-1, NULL, WNOHANG) > 0);  // 非阻塞清理僵尸子进程
+        }
     }
-
     close(server_fd);
     return 0;
 }
